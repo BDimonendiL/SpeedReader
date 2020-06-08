d42c69e052351857672c8167b4e9bafbdba8aaf2
Dmitry <forlandojack@gmail.com>
2020-06-09 02:34:02 +0300

    Recreating a project after getting a bug with UI QT Editor

--- SpeedReader/main.cpp
+++ SpeedReader/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    w.setFixedSize(w.height(), w.width());
+    w.setFixedSize(w.width(), w.height());
 
     w.show();
     return a.exec();
